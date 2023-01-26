867fb1f9a442c0c77063824c6a4d29d70e093ce2
WenjiaRan <399010199@qq.com>
2023-01-26 11:32:42 +0800

    取消了增加按钮, 将其逻辑合并到确认按钮中,确认之后自动创建一条新 的.增加了每分钟对ddl更新一次的功能,初步增加本地储存, 目前能够记录reminder条数

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+// initLocalStorage();
+
+// 打开页面时应该只有一条空的, 此时要利用本地储存将数据还原, 下午要学一下json
+
 let form = document.getElementsByTagName("form").item(0);
+let listNum = form.children.length;
+// let listNum = localStorage.clickcount
 
 function del(my) {
     // 通过my获取到要删除行 删除
@@ -8,28 +14,39 @@ function del(my) {
 function add() {
     var elem = document.createElement("div");
     elem.id = "1";
-    elem.innerHTML = "<button class=\"btn\" onclick=\"del(this)\">任务完成</button>\
-    <input type=\"text\">\n<label id=\"ddl-time\"></label>\n<input type=\"text\" id=\"ddl_time_text\" placeholder=\"格式:12/31/2024 00:00:00\">\
-    <button class=\"btn\" onclick=\"count_down(this)\">确认</button>";
+    elem.innerHTML = `<!-- 这个按钮按下去就可以删除该条ddl -->
+    <button class="btn" onclick="del(this)">任务完成</button>
+    <!-- 这是ddl内容 -->
+    <input type="text"  placeholder="ddl内容">
+    <!-- 距离ddl还有多少时间 -->
+    <label id="ddl-time"></label>
+    <input type="text" id="ddl_time_text" placeholder="格式:08/31/2024 00:00:00">
+    <button class="btn" onclick="count_down(this)">确认</button>
+    <!-- 在haha里存储ddl时间 -->
+    <label style="display:none"></label>`;
     form.appendChild(elem);
+    // add1();
 }
 
+// my是一个按钮
 function count_down(my) {
     //const endDate = '01/01/2024 00:00:00';
     const endDate = my.previousElementSibling.value
     // date format mm/dd/yyyy
 
-    // window.onload=function(){
-    //     //每1秒刷新时间
-    //   setInterval("cal_time()",1000);
-    // }
     var arr=cal_time(endDate);
     var [d,h,m,s]=[arr[0],arr[1],arr[2],arr[3]];
     //删除输入框和确认键 添加倒计时
-    my.previousElementSibling.previousElementSibling.innerHTML=`ddl在${d}天${h}小时${m}分之后, hurry up!`;
+    my.previousElementSibling.previousElementSibling.innerHTML=`ddl在${d}天${h}小时${m}分之后`;
+    my.nextElementSibling.innerHTML= my.previousElementSibling.value;
     (my.previousElementSibling).remove();
     (my).remove();
-    
+    // 不移除元素，只是让他隐形
+    // (my.previousElementSibling).setAttribute("style","display:none");
+    // (my).setAttribute("style","display:none");
+    // console.log( my.previousElementSibling.value);
+    //自动加一个
+    add();
 }
 
 function cal_time(endDate)
@@ -45,3 +62,48 @@ function cal_time(endDate)
     let s = Math.floor((distance % (1000 * 60)) / (1000));
     return [d,h,m,s];
 }
+
+function update(div)
+{
+    var endDate=div.children[3].innerHTML;
+    var arr=cal_time(endDate);
+    var [d,h,m,s]=[arr[0],arr[1],arr[2],arr[3]];
+    div.children[2].innerHTML=`ddl在${d}天${h}小时${m}分之后`;
+}
+
+//每分钟更新一次倒计时
+window.onload=function()
+{
+  setInterval("UpdateCountDownTime()",1000*60);
+}
+
+function UpdateCountDownTime()
+{
+    listNum = form.children.length;
+    for(var i=0;i<listNum-1;i++)
+    {
+        // 参数是div
+        update(form.children[i]);
+    }
+}
+
+// 本地储存
+// function initLocalStorage()
+// {
+//     if(typeof(Storage) !== "undefined") {
+//         if (localStorage.clickcount) {
+            
+            
+//         } else {
+//             localStorage.clickcount = 1;
+//         }
+//         } else {
+//         document.getElementById("result").innerHTML = "抱歉！您的浏览器不支持 Web Storage ...";
+//     }
+// }
+
+// function add1()
+// {
+//     localStorage.clickcount = Number(localStorage.clickcount)+1;
+//     console.log(localStorage.clickcount);
+// }
